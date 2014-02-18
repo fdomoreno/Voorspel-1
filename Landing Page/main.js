cdd2a769b6e3b9deb702f56fe80fec98bc2142ed
maxtermax <esneyder_a@hotmail.com>
2014-02-18 18:05:38 -0500

    Creando el programa que agrega las imagenes de los equipos!!

--- Landing Page/main.js
+++ Landing Page/main.js
@@ -3,17 +3,21 @@ $(document).on('ready',function(){
 
 var obj=$("ol:nth-child(1)")
 ,	path="#Seccion-articulos ul"
+,	array=['green','black','red','olive','darkred']
+, 	indice=1
 
-//$("ol:nth-child("+i+")").children(".Equipo2").css({"background-image":"url("+ Equipo1[i] +")"});
-
-for( var i=0;i<5;i++ ){
+for( var i=0;i<array.length;i++ ){
 
 	obj.clone().appendTo(path);
+	indice++
+$("#Seccion-articulos ul ol:nth-child("+indice+")").children("div").css({"background":""+array[i]+""});
 
 
+}//add items
 
-}//termin iteresion
 
+//$("ol:nth-child("+i+")").children("div").css({"background":""+array[i]+""});
+//#Seccion-articulos ul ol:nth-child(1)
 
 
 
